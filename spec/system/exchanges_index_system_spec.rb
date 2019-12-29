0795c96f802f0619f121f46c49c128d0c5363d67
Gusta <lgx.0097@gmail.com>
2019-12-29 18:08:25 -0300

    tests exchange_index_system

---
+++ spec/system/exchanges_index_system_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Exchange Currency Process", :type => :system, js: true do
+  it "show exchange value" do
+    
+    visit '/'
+
+    within("#exchange_form") do
+      select('EUR', from: 'source_currency')
+      select('BRL', from: 'target_currency')
+      fill_in 'amount', with: '10'
+    end
+
+    click_button 'CONVERTER'
+
+    expect(page).to have_content("value")  
+  end
+end
