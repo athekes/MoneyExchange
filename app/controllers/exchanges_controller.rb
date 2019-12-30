d3f1c62c57da5c9e3864a1b7bd0f9d68890edf5b
Gusta <lgx.0097@gmail.com>
2019-12-30 13:22:57 -0300

    Ajax on exchange index and setup for codeship

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -3,7 +3,9 @@ class ExchangesController < ApplicationController
   end
 
   def convert
-    value = ExchangeService.new(params[:source_currency], params[:target_currency], params[:amount]).perform
-    render json: {"value": value}
+    @value = ExchangeService.new(params[:source_currency], params[:target_currency], params[:amount]).perform   
+    respond_to do |format|
+      format.js 
+    end
   end
 end
